cbb0e3dcb696caaef768c6be7c644e23fca7d55d
krast0 <devilmarti@abv.bg>
2022-04-11 15:34:34 +0300

    yea

---
+++ script.js
@@ -0,0 +1,8 @@
+function showpoop() {
+    let showpoopie = document.getElementById('text')
+    showpoopie.style.display = 'inline';
+    let textToBeDeleted = document.getElementById('awfull')
+    textToBeDeleted.style.display = "none"
+
+
+}
